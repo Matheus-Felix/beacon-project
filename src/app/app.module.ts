28ec3446f0e7443bd4e198b9c9da327797bf6472
Matheus-Felix <matheusgfelix@gmail.com>
2019-10-29 15:58:23 -0300

    Adding detail page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomePage } from './home/home.page';
+import { DetailPage } from './detail/detail.page';
 
 @NgModule({
-  declarations: [AppComponent,
-  HomePage],
-  entryComponents: [HomePage],
+  declarations: [AppComponent],
+  entryComponents: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
